794fb2372a0bfc297056cfa2a11dfe4f51b98091
Daan Schilder <daan.schilder@vanmoof.com>
2020-04-29 11:41:07 +0200

    Removed some verbose functionallity

--- main.js
+++ main.js
@@ -34,11 +34,6 @@ var argv = require('yargs')
 
 if(argv.l != undefined){
     var packentries = pack.list_contents(argv.l)
-    
-    packentries.forEach(function(value, number, array) {
-        value.imageInfo = get_electrified3_image_info_by_image(value.imageData)
-    })
-
     console.log(packentries)
 }
 else if(argv.u != undefined) {
@@ -81,37 +76,4 @@ function decimalToHexString(number)
   }
 
   return number.toString(16).toUpperCase();
-}
-
-function bytearray_readInt32(bytearray, offset) {
-    var bytes = bytearray.slice(offset, offset + 4)
-    return (bytes[3] << 24)  | (bytes[2] << 16)  | (bytes[1] << 8) | bytes[0]
-}
-
-function get_electrified3_image_info_by_image(image_bytearray) {
-    var hexstring = decimalToHexString(bytearray_readInt32(image_bytearray, 0))
-
-    var imageInfo = {}
-
-    if(hexstring == 'AA55AA55') {
-        if(image_bytearray[4] == 0xF4)
-        {
-            imageInfo.type = 'main'
-            imageInfo.version = image_bytearray[7] + '.' + image_bytearray[6] + '.' + image_bytearray[5]
-            return imageInfo;
-        }
-        if(image_bytearray[4] == 0xA1)
-        {
-            imageInfo.type = 'motor'
-            imageInfo.version = image_bytearray[7] + '.' + image_bytearray[6] + '.' + image_bytearray[5]
-            return imageInfo;
-        }        
-    }
-
-    else if(hexstring == '2044414F')
-    {
-        imageInfo.type = 'BLE'
-        imageInfo.version = image_bytearray[33] + '.' + image_bytearray[34] + '.' + image_bytearray[35]
-        return imageInfo
-    }
 }
